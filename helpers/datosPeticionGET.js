1c78ae02554254f7c763ebedc6732b197f10686d
206 <206@academicos.cesde.edu.co>
2022-09-05 12:11:34 -0500

    05/09/2022

--- helpers/datosPeticionGET.js
+++ helpers/datosPeticionGET.js
@@ -6,7 +6,7 @@ const ID_ARTISTA="6Unmr1mmDxRqZY7jkSQOcg"
 
 export const URI=`https://api.spotify.com/v1/artists/${ID_ARTISTA}/top-tracks?market=US`
 
-const TOKEN="Bearer BQCEqpxvRCiNpbojgLu_i0Pxu2Ys_n4YWQ6HS_V-Z7pR8ZG6q_0IDlnscozNn8jJ1SMK1SEQn4UzBX9-d2E2huWMTclujYhMmJbjJWcoQUMLWCl2eZAMzPf5VyetE-Sfuhi2QsbsE31nT8TydP4D4dgFMyG2IzHHW5zUdsT0YYMshfdFcLlnHS4hwv7Vd20"
+const TOKEN=" Bearer BQCU0ud_PkRLmIk43SGbbeYltUh2Og0KVTo3HRVTh1B73Vv081ejnkfVMf7ly88X5Ri84S6D55PMT4gd_i9jd0e5SkwwL4zJ_oUSu9_oUvk9_lIuRqL3M7lcjx4h-papQ2hgZcE9iIzZBi0mQLDoP8GNefie-Px2MZJGrhNyxnIMU5EhEwJLspiFIKv--oU"
 
 
 export  const PETICION={
--- src/services/servicioGenerarToken.js
+++ src/services/servicioGenerarToken.js
@@ -11,11 +11,35 @@ const DATO3="client_secret=851ccd383b15470083b4c51e2a438827"
 
 //PETICION
 
-const PETICION={{
-  method='post',
-  headers= {},
+const PETICION={
+  method:'POST',
+  headers: {"Content-Type" : "application/x-www-form-urlencoded"},
   body: DATO1+"&"+DATO2+"&"+DATO3
 }
 
 
 //FECTH
+
+//promesa 
+
+fetch(URI,PETICION)
+.then(function(respuesta){
+ return respuesta.json()
+})
+.then(function(respuesta){
+   console.log(respuesta)
+})
+.catch(function(respuesta){
+  console.log(respuesta)
+})
+
+//async await
+
+
+ async function obtenerToken(){
+  let respuesta = await fetch(URI,PETICION)
+
+  return await respuesta.json()
+}
+
+console.log( await obtenerToken())
